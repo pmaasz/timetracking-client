f23652491e4ed0b568ae51ea8cd0b46001caeb56
Philip Maaß <PhilipMaasz@aol.com>
2022-07-07 18:26:33 +0200

    adds window controls

--- main.js
+++ main.js
@@ -8,7 +8,17 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
+//open the window
 app.whenReady().then(() => {
     createWindow()
+
+    //Open a window if none are open (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 });
 
+//On Windows and Linux, exiting all windows generally quits an application entirely
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
