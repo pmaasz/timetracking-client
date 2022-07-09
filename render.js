dbe13285bc28609b5b20516f881dec7b1b26f85b
Philip Maaß <PhilipMaasz@aol.com>
2022-07-09 20:32:24 +0200

    displays current worked time when in active work mode

--- render.js
+++ render.js
@@ -7,7 +7,11 @@ const buttonContainer = document.getElementById('button-container');
 const clockContainer = document.getElementById('clock');
 const timerContainer = document.getElementById('timer');
 
-let timer = "00:00";
+let timer;
+let startTime;
+let pauseTime;
+let resumeTime;
+let endTime;
 let clock = new Date().toLocaleTimeString('de-DE', {
     hour12: false,
     hour: '2-digit',
@@ -24,6 +28,9 @@ buttonContainer.removeChild(endButton);
 content.removeChild(timerContainer);
 
 startButton.addEventListener('click', () => {
+    timer = "00:00";
+    startTime = new Date();
+
     buttonContainer.removeChild(startButton);
 
     buttonContainer.appendChild(pauseButton);
@@ -34,6 +41,7 @@ startButton.addEventListener('click', () => {
 });
 
 pauseButton.addEventListener('click', () => {
+    pauseTime = new Date();
     buttonContainer.removeChild(pauseButton);
     buttonContainer.removeChild(endButton);
 
@@ -45,6 +53,7 @@ pauseButton.addEventListener('click', () => {
 });
 
 resumeButton.addEventListener('click', () => {
+    resumeTime = new Date();
     buttonContainer.removeChild(resumeButton);
     buttonContainer.removeChild(endButton);
 
@@ -56,6 +65,7 @@ resumeButton.addEventListener('click', () => {
 });
 
 endButton.addEventListener('click', () => {
+    endTime = new Date();
     //if buttonContainer has child pausebutton, remove it
     if (buttonContainer.contains(pauseButton)) {
         buttonContainer.removeChild(pauseButton);
@@ -73,16 +83,35 @@ endButton.addEventListener('click', () => {
     content.appendChild(clockContainer);
 });
 
-function setTimer() {
-    timerContainer.innerText = timer;
+function toHHMM (timer) {
+    timer = timer / 1000;
+    let sec_num = parseInt(timer, 10);
+    let hours   = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+
+    return hours+':'+minutes;
 }
 
 function getTime() {
-    clock = new Date().toLocaleTimeString('de-DE', {
+    clock = new Date();
+    clockContainer.innerHTML = clock.toLocaleTimeString('de-DE', {
         hour12: false,
         hour: '2-digit',
         minute: '2-digit',
     });
-    clockContainer.innerHTML = clock;
+
+    if (startTime && !pauseTime) {
+        timer = clock.getTime() - startTime.getTime();
+    } else if (resumeTime) {
+        timer = (clock.getTime() - startTime.getTime()) + (clock.getTime() - resumeTime.getTime()) - (resumeTime.getTime() - pauseTime.getTime());
+        timer.toString();
+    }
+
+    if(timer){
+        timerContainer.innerHTML = toHHMM(timer);
+    }
 }
 setInterval(getTime, 1000);
