06e80c808b11e7ba2030d30b1057f61a9bad0415
Philip Maaß <PhilipMaasz@aol.com>
2022-07-09 18:14:55 +0200

    changes styles

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 750,
-        height: 400
+        width: 700,
+        height: 350
     })
 
     win.loadFile('index.html')
